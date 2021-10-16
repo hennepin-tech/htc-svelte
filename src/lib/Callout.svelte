29346606727da5a45c200ba6bffe8bc4d9653d12
Jacob Stordahl <jacob@stordahl.dev>
2021-10-16 18:27:51 -0500

    Makes callout keyboard accessible

--- src/lib/Callout.svelte
+++ src/lib/Callout.svelte
@@ -7,16 +7,20 @@
     path: '/',
     title: 'Learn More'
   }
+
+  const goto = (path) => {
+    window.location = path
+  }
 </script>
 
-<article class="callout">
+<button class="callout" on:click={() => goto(props.path)}>
   <a href={props.path}>
     <img class="callout__image" src={props.image} alt={props.imageAlt}>
     <header class="callout__header">
       <h2 class="callout__title">{props.title}</h2>
     </header>
   </a>
-</article>
+</button>
 
 <style>
   .callout {
@@ -24,7 +28,25 @@
     height: 300px;
     overflow: hidden;
     margin: auto;
+    padding: 0;
+    border: none;
     border-radius: 5px;
+    transition: all 0.1s ease-in-out 0s;
+  }
+
+  .callout:hover {
+    transform: scale(0.99);
+  }
+
+  .callout:focus {
+    outline: var(--focus-border);
+    outline-offset: 2px;
+    transform: scale(0.99);
+  }
+  .callout:active {
+    outline: var(--focus-border);
+    outline-offset: 2px;
+    transform: scale(0.98);
   }
 
   .callout > a {
@@ -34,6 +56,7 @@
 
   .callout__image {
     height: 100%;
+    width: 100%;
   }
 
   .callout__header {
