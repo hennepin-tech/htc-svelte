8f3e19c988c1e413a9748fb06680a4c069a92ab1
Jacob Stordahl <jacob@stordahl.dev>
2021-10-21 15:35:29 -0500

    Adds named exports to lib

---
+++ src/lib/index.ts
@@ -0,0 +1,30 @@
+// Global Styles
+export { default as styles } from './global.module.css'
+
+// Components
+export { default as Accordion } from './Accordion.svelte'
+export { default as Banner } from './Banner.svelte'
+export { default as Button } from './Button.svelte'
+export { default as Callout } from './Callout.svelte'
+export { default as Modal } from './Modal.svelte'
+export { default as Toast } from './Toast.svelte'
+
+// Site Components
+export { default as Header } from './site/header/Header.svelte'
+export { default as Footer } from './site/footer/Footer.svelte'
+
+// Stores
+export { innerWidth } from './stores/innerWidth'
+
+// Utils
+export { mergeById } from './utils/mergeById'
+
+// Icons
+export { default as ArrowD } from './icons/Arrow-d.svelte'
+export { default as ArrowL } from './icons/Arrow-l.svelte'
+export { default as ArrowR } from './icons/Arrow-r.svelte'
+export { default as ArrowU } from './icons/Arrow-u.svelte'
+export { default as Book } from './icons/Book.svelte'
+export { default as Close } from './icons/Close.svelte'
+export { default as Person } from './icons/Person.svelte'
+export { default as Search } from './icons/Search.svelte'
