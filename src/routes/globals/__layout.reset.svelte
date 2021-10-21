9c1ff099d4b29a5b9911b83954ec28969d722da5
Jacob Stordahl <jacob@stordahl.dev>
2021-10-21 12:34:22 -0500

    set innerWidth in globals layout.reset

--- src/routes/globals/__layout.reset.svelte
+++ src/routes/globals/__layout.reset.svelte
@@ -1,7 +1,14 @@
 <script lang=ts>
-   import Header from '$lib/site/header/Header.svelte'
-   import Footer from '$lib/site/footer/Footer.svelte'
+  import Header from '$lib/site/header/Header.svelte'
+  import Footer from '$lib/site/footer/Footer.svelte'
+
+  import { innerWidth } from '$lib/stores/innerWidth'
+  let localWidth  
+  $: innerWidth.set(localWidth)
 </script>
+
+<svelte:window bind:innerWidth={localWidth}/>
+
 
 <Header />
   <slot/>
